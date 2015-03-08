1961ba7813d9c6314fcd03d97b83c33820e3a7b8
Colin Prieto <colin.prieto@gmail.com>
2015-03-09 00:58:25 +0100

    MAJ methode get liste notifs

--- serveurNotification.java
+++ serveurNotification.java
@@ -1,10 +1,14 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 
-public class serveurNotification extends UnicastRemoteObject implements intServeurNotification{
+public class serveurNotification extends UnicastRemoteObject implements IntServeurNotification{
 
 	/**
 	 * 
@@ -17,9 +21,27 @@ public class serveurNotification extends UnicastRemoteObject implements intServe
 		super();
 		this.listeNotification = new HashMap<String, Notification>();
 	}
-
+	
+	public String getListeNotifications (String utilCible)
+	{
+		ArrayList<String> notifs = null;
+		for(Entry<String, Notification> entry : this.listeNotification.entrySet()){
+		    if(entry.getKey()==utilCible)
+		    {
+		    	notifs.add(entry.getValue().toString());
+		    }
+		}
+		Object tab[] =  notifs.toArray();
+		String liste = "";
+		int i;
+		for(i=0;i<tab.length;i++){
+			liste += (String) "\n"+tab[i];
+		}
+		return liste;
+	}
+	
 	@Override
-	public String creerNotification(String utilSource, String utilCibe,
+	public String creerNotification(String utilSource, String utilCible,
 			String competence, String type) {
 		// TODO Auto-generated method stub
 		return null;
