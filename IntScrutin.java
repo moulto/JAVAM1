49d87bfa9b34aeb2145ee87e50adcd08f75933c8
PRIETO Florian <florian.prieto@gmail.com>
2015-03-31 17:41:29 +0200

    Delete IntScrutin.java

--- IntScrutin.java
+++
@@ -1,8 +0,0 @@
-import java.net.MalformedURLException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IntScrutin extends Remote{
-	public void voter(String votant, Integer voix) throws RemoteException, MalformedURLException;
-	public void terminerScrutin() throws RemoteException, MalformedURLException;
-}
